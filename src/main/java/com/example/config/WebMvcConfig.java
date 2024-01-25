b2c1d6f4243e57c337badbfa052c4edfb24920a1
Nick Avlonitis <avlonitis.nick@mayo.edu>
2024-01-25 10:20:38 -0600

    clean

--- src/main/java/com/example/config/WebMvcConfig.java
+++ src/main/java/com/example/config/WebMvcConfig.java
@@ -7,18 +7,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-//@EnableWebMvc
-//public class WebMvcConfig implements WebMvcConfigurer {
 public class WebMvcConfig {
-
-//  @Bean
-//  public ViewResolver templateResolver() {
-//    ServletContextTemplateResolver bean = new ServletContextTemplateResolver();
-//    bean.setPrefix("/WEB-INF/views/");
-//    bean.setSuffix(".html");
-//    bean.setViewNames("thymeleaf/*");
-//    return bean;
-//  }
 
   @Bean
   public ViewResolver jspViewResolver() {
@@ -29,17 +18,4 @@ public class WebMvcConfig {
     bean.setViewNames("jsp/*");
     return bean;
   }
-//  @Bean
-//  public ViewResolver viewResolver() {
-//    final InternalResourceViewResolver bean = new InternalResourceViewResolver();
-//    bean.setViewClass(JstlView.class);
-//    bean.setPrefix("/WEB-INF/jsp/");
-//    bean.setSuffix(".jsp");
-//    return bean;
-//  }
-//
-//  @Override
-//  public void configureViewResolvers(ViewResolverRegistry registry) {
-//    registry.viewResolver(viewResolver());
-//  }
 }
--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -4,7 +4,9 @@ import com.example.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -19,11 +21,6 @@ public class LoginController {
   public String showLoginPage(ModelMap model){
     return "jsp/login";
   }
-
-//  @GetMapping(value="/hello")
-//  public String hello(ModelMap model){
-//    return "hello";
-//  }
 
   @PostMapping(value="/login")
   public String showWelcomePage(ModelMap model, @RequestParam String name, @RequestParam String password){
